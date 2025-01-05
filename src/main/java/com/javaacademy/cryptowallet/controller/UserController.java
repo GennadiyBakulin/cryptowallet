d137dc54a5c4e8ba8cf6583b75168f20654c3f7a
Gennadiy Bakulin <g.bakulin@yandex.ru>
2025-01-05 21:39:29 +0700

    Swagger описание методов контроллера UserController

--- src/main/java/com/javaacademy/cryptowallet/controller/UserController.java
+++ src/main/java/com/javaacademy/cryptowallet/controller/UserController.java
@@ -3,6 +3,9 @@ package com.javaacademy.cryptowallet.controller;
 import com.javaacademy.cryptowallet.dto.user.CreateUserDto;
 import com.javaacademy.cryptowallet.dto.user.ResetPasswordUserDto;
 import com.javaacademy.cryptowallet.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Tag(name = "Контроллер пользователя", description = "Контроллер для работы с пользователем")
 public class UserController {
 
   private final UserService userService;
 
+  @Operation(
+      summary = "Регистрация пользователя",
+      description = "Регистрирует нового пользователя"
+  )
+  @ApiResponse(responseCode = "202", description = "Создан")
   @PostMapping("/signup")
   @ResponseStatus(code = HttpStatus.CREATED)
-  public void registration(@RequestBody CreateUserDto newUser) {
+  public void signup(@RequestBody CreateUserDto newUser) {
     userService.saveUser(newUser);
   }
 
+  @Operation(
+      summary = "Смена пароля пользователя",
+      description = "Меняет пароль у зарегистрированного пользователя"
+  )
+  @ApiResponse(responseCode = "200", description = "Успешно")
   @PostMapping("/reset-password")
   @ResponseStatus(code = HttpStatus.OK)
   public void resetPassword(@RequestBody ResetPasswordUserDto resetPassword) {
